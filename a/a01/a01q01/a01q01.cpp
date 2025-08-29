18a3348a8aee9797f944419304c9e5ee5fae7c7e
landisTEMPR <bglandis1@cougars.ccis.edu>
2025-08-29 12:31:32 -0500

    init

---
+++ a/a01/a01q01/a01q01.cpp
@@ -0,0 +1,20 @@
+// File: a01q01.cpp
+// Author: Brysen Landis
+
+#include <iostream>
+
+int main()
+{
+    int a = 0, b = 0, c = 0, d = 0, e = 0, f = 0, g = 1;
+    std::cin >> a >> b >> c >> d >> e >> f >> g;
+// Declare your temporary variables below.
+// For instance if you need three temporary variables below you
+// should have "int t0, t1, t2;" below this line.
+// REPLACE WITH YOUR WORK for
+// a = a + b - c * d + e * f / g;
+// Number of temporary variables: ___
+    std::cout << a << ' ' << b << ' ' << c << ' '
+              << d << ' ' << e << ' ' << f << ' '
+              << g << std::endl;
+    return 0;
+}
---
+++ notes/notes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+void isprime(int x[], int primes)
+{
+    
+}
+
+int main()
+{
+    int x[] = {};
+    
+    
+    
+    return 0;
+}
