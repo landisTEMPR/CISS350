d7b38c2cfb8899ba6e95d261ebd46861af071c59
landisTEMPR <bglandis1@cougars.ccis.edu>
2025-09-05 11:38:51 -0500

    matrix

--- notes/matrix/Mat.h
+++ notes/matrix/Mat.h
@@ -8,6 +8,9 @@ class Mat
   public:
     Mat(int, int);
     ~Mat();
+    int nrows() const;
+    int ncols() const;
+    double operator()(int, int) const;
   private:
     int nrows_;
     int ncols_;
--- notes/matrix/main.cpp
+++ notes/matrix/main.cpp
@@ -5,8 +5,16 @@ int main()
 {
     Mat m(3, 4);
     std::cout << m << '\n';
-    std::cout << m.nrows() << '\n';
-    std::cout << m.ncols() << '\n';
-    
+    m(0, 0) = 42; // m.operator()(0, 0) = 42;
+    // std::cout << m.nrows() << '\n';
+    // std::cout << m.ncols() << '\n';
+
+    // p = new Mat(5, 10);
+
+    // ...
+
+    // delete p; destructor of p at memory address
+    // returns the memory allocated to the variable
+    // back to the heap.
     return 0;
-}f
+}
